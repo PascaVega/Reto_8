f23e9c54280851e9d18d1a31d47b3d4e20431997
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-28 17:41:19 -0500

    #RETO 8 - Punto 6.py

--- #RETO 8 - Punto 6.py
+++ #RETO 8 - Punto 6.py
@@ -2,23 +2,27 @@
 #Leer un número natural n, leer otro dato de tipo real x y calcular x^n usando ciclos for.
 
 def introducir():
+    #Se incgresan los dos números a operar
     base : float = float(input("Introduzca la base. Ejemplo: 5: "))
     exponente : int = int(input("Introduzca el exponente. Ejemplo: 5: "))
     desarrollo(base,exponente)
 
 def desarrollo(base,exponente):
+    #Se define una nueva variable que va a expresar el resultado
     potencia : float = 1
+    #El ciclo se basa en una multiplicación reiterada
     for i in range(1, exponente+1):
         potencia *=base
     print(f"{base} elevado a {exponente} es {potencia}")
         
 def continuar():
+    #El usuario decide si desea volver a correr el programa
     opcion : int = int(input("¿Desea continuar? Marque 1 (sí) o 2 (no): "))
     return opcion
 
 if __name__ == "__main__":
     print("Programa para calcular el valor de x^n.")
-
+    #El programa continuará repitiendose hasta que el usuario lo desee
     while True:
         introducir()
         opcion = continuar()
