ce7cbf7391dc1d10cbc12bc9517bda9161c50e7c
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-28 17:37:28 -0500

    #RETO 8 - Punto 5.py

--- #RETO 8 - Punto 5.py
+++ #RETO 8 - Punto 5.py
@@ -2,23 +2,27 @@
 #Calcular el valor de 2 elevado a la potencia n usando ciclos for.
 
 def introducir():
+    #Se introduce el número
     numero : int = int(input("Introduzca el exponente. Ejemplo: 5: "))
     desarrollo(numero)
 
 def desarrollo(numero):
+    #Se define una nueva variable
     potencia : int = 1
+    #En el ciclo se realiza una multiplicación reiterativa para hallar la potencia
     for i in range(1, numero+1):
         potencia *=2
     print(f"2 elevado a {numero} es {potencia}")
         
 
 def continuar():
+    # El usuario decide si repetir el programa
     opcion : int = int(input("¿Desea continuar? Marque 1 (sí) o 2 (no): "))
     return opcion
 
 if __name__ == "__main__":
     print("Programa para calcular el valor de 2 elevado a un número dado")
-
+    #El ciclo se repetirá tantas veces como el usuario desee
     while True:
         introducir()
         opcion = continuar()
